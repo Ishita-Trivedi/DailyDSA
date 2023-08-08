f5bd185954cefd55c5832f8d32644538f3945867
Ishita <103136112+Ishita-Trivedi@users.noreply.github.com>
2023-08-08 23:41:14 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -7,26 +7,27 @@ class Solution{
 
 	public:
 	const int mod = static_cast<int>(1e9 + 7);
-	int helper(int i,int target,int n,int arr[],vector<vector<int>>&dp){
-	    if(i>=n){
-	        if(target==0)
-	        return 1;
-	        return 0;
-	    }
-	    if(dp[i][target]!=-1)return dp[i][target];
-	    int take = 0;
-        if (target - arr[i] >= 0)
-        take=helper(i+1,target-arr[i],n,arr,dp)%mod;
-	    int ntake=helper(i+1,target,n,arr,dp)%mod;
-	    return dp[i][target]=(take+ntake)%mod;
-	}
 	int perfectSum(int arr[], int n, int sum)
 	{
-	    vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return helper(0,sum,n,arr,dp);
+	    vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+	    for(int i=0;i<=n;i++)
+        dp[i][0]=1;
+    for (int i = 1; i <=n; i++) {
+        for (int target = 0; target <= sum; target++) { // Add increment statement
+           
+            int take = 0;
+            if (target - arr[i-1] >= 0)
+                take = (dp[i - 1][target - arr[i-1]]) % mod;
+            int ntake = (dp[i - 1][target]) % mod;
+            dp[i][target] = (take + ntake) % mod;
+            
+        }
+    }
+    return dp[n ][sum];
 	}
 	  
 };
+
 
 //{ Driver Code Starts.
 int main() 

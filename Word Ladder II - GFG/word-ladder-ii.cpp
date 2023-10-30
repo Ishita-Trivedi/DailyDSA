d38387afa6f92a334dc58a4e9e99e55f4facf89a
Ishita <103136112+Ishita-Trivedi@users.noreply.github.com>
2023-10-30 15:46:30 +0530

    Added solution - LeetHub

--- Word Ladder II - GFG/word-ladder-ii.cpp
+++ Word Ladder II - GFG/word-ladder-ii.cpp
@@ -9,78 +9,36 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
-       // Push all values of wordList into a set
-        // to make deletion from it easier and in less time complexity.
-        unordered_set<string> st(wordList.begin(), wordList.end());
-        
-        // Creating a queue ds which stores the words in a sequence which is
-        // required to reach the targetWord after successive transformations.
-        queue<vector<string>> q;
-
-        // BFS traversal with pushing the new formed sequence in queue 
-        // when after a transformation, a word is found in wordList.
-
+        vector<vector<string>>ans;
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        vector<string>usedOnLevel;int level=0;
+        queue<vector<string>>q;
         q.push({beginWord});
-
-        // A vector defined to store the words being currently used
-        // on a level during BFS.
-        vector<string> usedOnLevel;
         usedOnLevel.push_back(beginWord);
-        int level = 0;
-       
-        // A vector to store the resultant transformation sequence.
-        vector<vector<string>> ans;
-        while (!q.empty())
-        {
-            vector<string> vec = q.front();
-            q.pop();
-
-            // Now, erase all words that have been
-            // used in the previous levels to transform
-            if (vec.size() > level)
-            {
+        while(!q.empty()){
+            vector<string> vec=q.front();q.pop();
+            if(vec.size()>level){
                 level++;
-                for (auto it : usedOnLevel)
-                {
-                    st.erase(it);
-                }
+                for(auto it:usedOnLevel)
+                st.erase(it);
             }
-
-            string word = vec.back();
-
-            // store the answers if the end word matches with targetWord.
-            if (word == endWord)
-            {
-                // the first sequence where we reached end
-                if (ans.size() == 0)
-                {
-                    ans.push_back(vec);
-                }
-                else if (ans[0].size() == vec.size())
-                {
-                    ans.push_back(vec);
-                }
+            string word=vec.back();
+            if(word==endWord){
+                if(ans.size()==0||vec.size()==ans[0].size())
+                ans.push_back(vec);
             }
-            for (int i = 0; i < word.size(); i++)
-            {   
-                // Now, replace each character of ‘word’ with char
-                // from a-z then check if ‘word’ exists in wordList.
-                char original = word[i];
-                for (char c = 'a'; c <= 'z'; c++)
-                {
-                    word[i] = c;
-                    if (st.count(word) > 0)
-                    { 
-                        // Check if the word is present in the wordList and
-                        // push the word along with the new sequence in the queue.
+            for(int i=0;i<word.length();i++){
+                char org=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.count(word)>0){
                         vec.push_back(word);
-                        q.push(vec);
-                        // mark as visited on the level
                         usedOnLevel.push_back(word);
+                        q.push(vec);
                         vec.pop_back();
                     }
                 }
-                word[i] = original;
+                word[i]=org;
             }
         }
         return ans;
